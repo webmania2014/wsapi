b67e89f3a79d3548138ba9f19ca4b3af7e66634c
webmania2014 <panda1987417@hotmail.com>
2015-02-03 17:05:17 +0100

    Revert "first commit"
    This reverts commit b90531f13dbb8998ba3fd634a25bed114aba64ba.

--- admin/page_inc/footer.php
+++
@@ -1,9 +0,0 @@
-	<script>
-		jQuery(document).ready(function(){
-			var doc_height = jQuery( document ).height();
-			var header_height = jQuery( 'header-table' ).height();
-			jQuery( "#main-table" ).css( 'height', doc_height - header_height );
-		});
-	</script>
-	</body>
-</html>
--- modules/module_index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once( $modulePath . '/config.php' );
-	require_once( $modulePath . '/db.php' );
-	require_once( $modulePath . '/global.php' );
-	require_once( $modulePath . '/variable.php' );
-	require_once( $modulePath . '/function.php' );
-	require_once( $modulePath . '/phpseclib/SSH2.php' );
-?>
